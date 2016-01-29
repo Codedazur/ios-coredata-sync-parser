1ddaf3b9a3003c8b5a4c671fe5614c826b6b720d
tamarabernad <tamara@codedazur.es>
2016-01-29 16:10:45 +0100

    first commit

---
+++ Example/Pods/Headers/Private/CDASyncCoreDataParser/CDASyncCoreDataStack.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/CDASyncCoreDataStack.h
--- Example/Pods/Target Support Files/CDASyncCoreDataParser/CDASyncCoreDataParser-umbrella.h
+++ Example/Pods/Target Support Files/CDASyncCoreDataParser/CDASyncCoreDataParser-umbrella.h
@@ -1,7 +1,7 @@
 #import <UIKit/UIKit.h>
 
 #import "CDAAbstractCoreDataParser.h"
-#import "SEACoreDataStack.h"
+#import "CDASyncCoreDataStack.h"
 
 FOUNDATION_EXPORT double CDASyncCoreDataParserVersionNumber;
 FOUNDATION_EXPORT const unsigned char CDASyncCoreDataParserVersionString[];
